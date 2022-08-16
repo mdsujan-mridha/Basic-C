65ecd96a529e94429d8d5306f7799187328c9f48
mdsujan-mridha <mdsujan134523@gmail.com>
2022-08-17 02:15:09 +0600

    pattern print 1

---
+++ Start Print/Star Pattern 1/StarPattern1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int j,i;
+           for(i=1;i<=5;i++){
+           	 for(j=1;j<=5;j++){
+           	        if(j<=i)
+           	        	cout<<"*";
+           	        else
+					    cout<<" " ;	
+			   }
+			    cout<<"\n";
+		   }
+           	
+		 
+  return 0;
+}
