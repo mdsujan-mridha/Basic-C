13066273265769a1ca3a4ef99fea288fe095f0fc
mdsujan-mridha <mdsujan134523@gmail.com>
2022-08-17 22:19:36 +0600

    start print 3 & 4

--- Start Print/Start Pattern 4/StarPattern 4 .cpp
+++ Start Print/Start Pattern 4/StarPattern 4 .cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
 	int j,i;
            for(i=1;i<=5;i++){
-           	 for(j=1;j<=5;j++){
-           	        if(j>=6-i)
+           	 for(j=1;j<=9;j++){
+           	        if(j>=6-i && j<=4+i)
            	        	cout<<"*";
            	        	
            	        else
--- Start Print/star pattern 3/StarPattern 3 .cpp
+++ Start Print/star pattern 3/StarPattern 3 .cpp
@@ -5,7 +5,7 @@ int main() {
 	int j,i;
            for(i=1;i<=5;i++){
            	 for(j=1;j<=5;j++){
-           	        if(j>=6-i)
+           	        if(j<=6-i)
            	        	cout<<"*";
            	        	
            	        else
